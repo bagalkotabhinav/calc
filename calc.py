d9bd79a1018fbe67f96d761c639e62facfceb154
Abhinav Bagalkot <abhinavb.is21@rvce.edu.in>
2026-02-05 20:56:07 +0530

    Function for addition

--- calc.py
+++ calc.py
@@ -1,8 +1,2 @@
-#THIS IS A CALCULATOR
-
-a=int(input("Enter No. 1: "))
-b=int(input("Enter No. 2: "))
-c=a+b
-print("Addition is: ", c)
-d=a*b
-print("Multiplication is: ", d)
+def add(a,b):
+    return a+b
